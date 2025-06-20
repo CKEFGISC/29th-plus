5667d9f8bc5146e3d475134a6525d7e2c1ea72eb
Aaron Wu <demonaarwu@tuta.io>
2025-06-20 23:17:40 +0800

    feat: create footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    const year = new Date().getFullYear();
+</script>
+
+<footer>
+    <p>Copyright © {year} CKEFGISC 建北電資 29th </p>
+</footer>
+
+<style>
+    * {
+        font-family: var(--font-1);
+    }
+    p {
+        text-align: center;
+        font-size: var(--text-base);
+        color: darkgray;
+    }
+    footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100vw;
+        margin: var(--spacing-6) auto;
+    }
+
+    @media (max-width: 767px) {
+        p {
+            font-size: var(--text-sm);
+        }
+    }
+</style>
